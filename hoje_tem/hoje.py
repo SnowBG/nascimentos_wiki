fdc60383c088e5d77bb1e8f8825ef03093a587f3
snowbg <snowbg@gmail.com>
2023-04-25 22:54:17 -0300

    Added .gitignore, docstrings and minor refactoring.

--- hoje_tem/hoje.py
+++ hoje_tem/hoje.py
@@ -15,24 +15,36 @@ from webdriver_manager.chrome import ChromeDriverManager
 from hoje_tem.transforma_data import TransformaData
 
 
-
-
 def data_atual():
-
+    """
+    Pega a data atual e a transforma em uma data por extenso.
+    """
     data = dt.today().strftime('%d-%m-%Y')
-    hoje = TransformaData().dataExtensoBr(data)
-    return hoje
+    return TransformaData().dataExtensoBr(data)
+    
 
 def monta_url():
-    return f"https://pt.wikipedia.org/wiki/{data_atual()}"
+    """
+    Gera uma URL para a Wikipedia com base na data atual.
+    """
+    dia = data_atual()
+    return f"https://pt.wikipedia.org/wiki/{dia}"
 
 def motorista():
+    """
+    Instancia o driver do navegador, busca o DOM da URL passada e 
+    retorna o objeto referente à página.
+    """
     url = monta_url()
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get(url)
     return driver
 
 def nascidos():
+    """
+    Busca dentro do DOM os dados referentes aos nascimentos dentro da página
+    e retorna os nomes e os anos de nascimento das pessoas.
+    """
     datas = ""
     elementos = motorista().find_elements(
                                         By.XPATH, 
@@ -44,10 +56,11 @@ def nascidos():
         datas += elemento.text
     return datas
 
-def check_status():
-    status = requests.get(monta_url())
-    return status
+def verifica_status():
+    """ Verifica se o HTTP status é 200 - OK. """
+    return requests.get(monta_url())
+
 
 if __name__ == "__main__":
-    assert check_status().status_code == 200
+    assert verifica_status().status_code == 200
     print(nascidos())
